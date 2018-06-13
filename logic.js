cff2570b5e270717bf9ec9ec0bdabc6d0ee2582d
Brad Said <bradsaid@gmail.com>
2018-06-13 17:06:45 -0500

    initial commit

---
+++ logic.js
@@ -0,0 +1,54 @@
+
+let config = {
+  apiKey: "AIzaSyDUkcRwJRJl8CNtm_PY66c9IvciRRlDZTI",
+  authDomain: "myapp-85244.firebaseapp.com",
+  databaseURL: "https://myapp-85244.firebaseio.com",
+  projectId: "myapp-85244",
+  storageBucket: "myapp-85244.appspot.com",
+  messagingSenderId: "547867111421"
+};
+firebase.initializeApp(config);
+
+let database = firebase.database();
+
+let train = "";
+let destination = "";
+let trainTime = 0;
+let frequency = 0;
+
+
+database.ref().child("trains").on("value", function(snapshot) {  // ttrying to set path to pull  LEFT OFF HERE
+  if (snapshot.child("train").exists() && snapshot.child("frequency").exists() && snapshot.child("time").exists() && snapshot.child("destination").exists()) {
+    train = snapshot.val().train;
+    destination = snapshot.val().destination;
+    trainTime = parseInt(snapshot.val().time);
+    frequency = parseInt(snapshot.val().frequency);
+    console.log(train);
+  }
+  /// a for loop here to add each table row?
+  $("#trains").text(train);
+  $("#destinations").text(destination);
+  $("#frequencies").text(frequency);
+  $("#traintimes").text(trainTime);
+
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
+$("#submit-bid").on("click", function(event) {
+  event.preventDefault();
+  // Get the input values
+  let train1 = $("#train-name").val().trim();  
+  let destination = $("#destination").val().trim();
+  let trainTime = parseInt($("#train-time").val().trim());
+  let frequency = parseInt($("#frequency").val().trim());
+      database.ref('trains/').set({
+      train: train1,
+      frequency: frequency,
+      destination : destination,
+      time : trainTime
+    });
+
+  
+});
